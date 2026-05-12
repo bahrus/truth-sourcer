6c16dead7bf3e7c11f7ca48be5d395f112c08699
bahrus <anderson.bruce.b@gmail.com>
2026-05-11 20:51:13 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
 # truth-sourcer
+
 Custom element feature that manages source of truth attributes
 
-With the advant of custom state 
+With the advant of custom state that can be set via internals, the need to reflect properties to attributes has greatly diminished.  In the view of this package, ideally attributes should be used only for initial configuration sent via server-side rendering.
+
+However, there are some limited use cases where "source of truth" attributes are still needed, so this package contains the world's first "custom element feature" to help manage such attributes, where the value of the attribute exactly mirrors a property with the same name and vice versa.
+
+To use this feature, say you are defining your custom element:
+
+
+```JavaScript
+import {TruthSourcer} from 'truth-sourcer/TruthSourcer.js';
+import 'assign-gingerly/object-extension.js';
+
+class MyElement extends HTMLElement {
+    #internals;
+    /**
+     * @type {EventTarget}
+     **/
+    propagator = new EventTarget();
+
+
+    static supportedFeatures = {
+        truthSourcer: {
+            fallbackSpawn: TruthSourcer,
+        }
+    };
+
+    /**
+     * @type {string}
+     **/
+    #name = '';
+
+    get name(){
+        return this.#name
+    }
+
+    set name(nv){
+        this.#name = nv;
+        this.propagator.dispatchEvent(new Event('name'));
+    }
+
+    static observedAttributes = ['name'];
+
+    constructor(){
+        super();
+        this.truthSourcer.hostPropagator = this.propagator;
+    }
+
+    attributeChangedCallback(name, oldValue, newValue){
+        this.truthSource.attributeChangedCallback(name, oldValue, newValue);
+    }
+}
+
+customElements.assignFeatures(MyElement, {
+    truthSourcer: { spawn: TruthSourcer }
+});
+
+customElements.define('my-element', MyElement);
+```
+
+Restrictions:
+
+Even though attributes are case insensitive, to inform truthSourcer the name of the property corresponding to the attribute, make sure observedAttributes gives the name of the property with proper casing.  This essentially means no "dash" support for such source of truth attributes.  
+
+Recommendation is to limit such cases to attributes supported natively by the platform (such as "name").
+
+The property values have to be initialized to non null values, so that truthSourcer can infer the type that the property should take (string, boolean, number).  This pretty much mirrors how the platform handles truth of source attributes.
+
+
---
+++ TruthSourcer.js
@@ -0,0 +1,169 @@
+// @ts-check
+/** @import {TruthSourcerProps, AllProps, FeatureSpawnContext} from './types/truth-sourcer/types' */
+
+/**
+ * Custom element feature that manages "source of truth" attributes.
+ * Keeps attribute values in sync with their corresponding properties
+ * and vice versa.
+ * 
+ * @implements {TruthSourcerProps}
+ */
+class TruthSourcer {
+    /** @type {WeakRef<HTMLElement> | null} */
+    #hostRef = null;
+
+    /** @type {string[]} */
+    #observedAttributes = [];
+
+    /** @type {AbortController | null} */
+    #abortController = null;
+
+    /** @type {EventTarget | null} */
+    #hostPropagator = null;
+
+    /** @type {boolean} */
+    #syncing = false;
+
+    /**
+     * @param {HTMLElement} hostElement
+     * @param {FeatureSpawnContext} ctx
+     * @param {Partial<TruthSourcerProps>} [initVals]
+     */
+    constructor(hostElement, ctx, initVals) {
+        this.#hostRef = new WeakRef(hostElement);
+        const ctor = /** @type {any} */ (hostElement.constructor);
+        this.#observedAttributes = ctor.observedAttributes || [];
+
+        if (initVals) {
+            if (initVals.hostPropagator) {
+                this.hostPropagator = initVals.hostPropagator;
+            }
+        }
+    }
+
+    /**
+     * The EventTarget the host uses to dispatch property change events.
+     * Setting this subscribes TruthSourcer to property change events
+     * for all observedAttributes.
+     */
+    get hostPropagator() {
+        return this.#hostPropagator;
+    }
+
+    /**
+     * @param {EventTarget | null} nv
+     */
+    set hostPropagator(nv) {
+        // Clean up previous listeners
+        if (this.#abortController) {
+            this.#abortController.abort();
+            this.#abortController = null;
+        }
+
+        this.#hostPropagator = nv;
+
+        if (nv) {
+            this.#abortController = new AbortController();
+            const signal = this.#abortController.signal;
+
+            for (const attrName of this.#observedAttributes) {
+                nv.addEventListener(attrName, () => {
+                    this.#syncPropertyToAttribute(attrName);
+                }, { signal });
+            }
+        }
+    }
+
+    /**
+     * Called by the host element's attributeChangedCallback.
+     * Syncs the attribute value into the corresponding property.
+     * 
+     * @param {string} name - The attribute name
+     * @param {string | null} oldValue - Previous attribute value
+     * @param {string | null} newValue - New attribute value
+     */
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (this.#syncing) return;
+
+        const host = this.#hostRef?.deref();
+        if (!host) return;
+
+        const currentValue = /** @type {any} */ (host)[name];
+        const coerced = this.#coerce(newValue, currentValue);
+
+        if (coerced !== currentValue) {
+            this.#syncing = true;
+            /** @type {any} */ (host)[name] = coerced;
+            this.#syncing = false;
+        }
+    }
+
+    /**
+     * Syncs a property value to its corresponding attribute.
+     * @param {string} attrName
+     */
+    #syncPropertyToAttribute(attrName) {
+        if (this.#syncing) return;
+
+        const host = this.#hostRef?.deref();
+        if (!host) return;
+
+        const value = /** @type {any} */ (host)[attrName];
+        this.#syncing = true;
+
+        if (typeof value === 'boolean') {
+            if (value) {
+                host.setAttribute(attrName, '');
+            } else {
+                host.removeAttribute(attrName);
+            }
+        } else if (value == null) {
+            host.removeAttribute(attrName);
+        } else {
+            host.setAttribute(attrName, String(value));
+        }
+
+        this.#syncing = false;
+    }
+
+    /**
+     * Coerces an attribute string value to the appropriate type
+     * based on the current property value's type.
+     * 
+     * @param {string | null} attrValue
+     * @param {any} currentValue - Used to infer the target type
+     * @returns {any}
+     */
+    #coerce(attrValue, currentValue) {
+        if (attrValue === null) {
+            // Attribute removed
+            if (typeof currentValue === 'boolean') return false;
+            if (typeof currentValue === 'number') return 0;
+            return '';
+        }
+
+        switch (typeof currentValue) {
+            case 'boolean':
+                // Presence of attribute = true (standard HTML behavior)
+                return true;
+            case 'number':
+                return Number(attrValue);
+            default:
+                return attrValue;
+        }
+    }
+
+    /**
+     * Clean up event listeners
+     */
+    dispose() {
+        if (this.#abortController) {
+            this.#abortController.abort();
+            this.#abortController = null;
+        }
+        this.#hostRef = null;
+        this.#hostPropagator = null;
+    }
+}
+
+export { TruthSourcer };
